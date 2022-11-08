21016c20abcc281c5c6ebf38591d996ca2528463
unknown <rubenvelazquez244@gmail.com>
2022-11-08 14:59:34 -0600

    Basic configurations than i need

---
+++ lua/rubens/keymaps.lua
@@ -0,0 +1,50 @@
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+--
+vim.keymap.set("n", "<C-b>", ':NvimTreeToggle<CR>', { silent = true })
+
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- Generate image of lines in a visual selection
+vim.keymap.set('v', '<Leader>s',  function() require('silicon').visualise_api() end,{desc="Take an screenshoet"} )
+-- Generate image of a whole buffer, with lines in a visual selection highlighted
+vim.keymap.set('v', '<Leader>bs', function() require('silicon').nvisualise_api({to_clip = true, show_buf = true}) end )
+-- Generate visible portion of a buffer
+vim.keymap.set('n', '<Leader>s',  function() require('silicon').visualise_api({to_clip = true, visible = true}) end )
+-- Generate current buffer line in normal mode
+vim.keymap.set('n', '<Leader>s',  function() require('silicon').nvisualise_api({to_clip = true}) end )
---
+++ lua/rubens/plugins.lua
@@ -0,0 +1,62 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	is_bootstrap = true
+	vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+	vim.cmd [[packadd packer.nvim]]
+end
+
+
+-- stylua: ignore start
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim' -- Package manager
+	use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Add git related info in the signs columns and popups
+	use 'nvim-treesitter/nvim-treesitter' -- Highlight, edit, and navigate code
+	use { 'nvim-treesitter/nvim-treesitter-textobjects', after = { 'nvim-treesitter' } } -- Additional textobjects for treesitter
+	use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+	use 'williamboman/mason.nvim' -- Manage external editor tooling i.e LSP servers
+	use 'williamboman/mason-lspconfig.nvim' -- Automatically install language servers to stdpath
+	use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } } -- Autocompletion
+	use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } } -- Snippet Engine and Snippet Expansion
+	use 'Mofiqul/dracula.nvim' -- Dracula theme
+	use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+	use { 'nvim-tree/nvim-tree.lua', requires = { 'nvim-tree/nvim-web-devicons' } } -- File explorer
+	use { 'numToStr/Comment.nvim' } -- Comment lines of code
+	use { "windwp/nvim-autopairs" } -- Close automatically pairs like ( [ {
+	-- Fuzzy Finder (files, lsp, etc)
+	use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+
+	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
+
+	use { "github/copilot.vim" } -- Github copilot suggest code 
+	
+	use { 'segeljakt/vim-silicon' }
+
+	if is_bootstrap then
+		require('packer').sync()
+	end
+end)
+-- stylua: ignore end
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+	print '=================================='
+	print '    Plugins are being installed'
+	print '    Wait until Packer completes,'
+	print '       then restart nvim'
+	print '=================================='
+	return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+	command = 'source <afile> | PackerCompile',
+	group = packer_group,
+	pattern = vim.fn.expand '$MYVIMRC',
+})
